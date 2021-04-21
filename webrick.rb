29b56c35a5fd2305268a8ebb1448c9877173f0bb
DaichiSaito <hrkedz@gmail.com>
2021-04-21 09:57:20 +0900

    add 動的ページ

--- webrick.rb
+++ webrick.rb
@@ -7,4 +7,14 @@ server = WEBrick::HTTPServer.new({
   :Port => 8000
 })
 
+server.mount_proc("/time") do |req, res|
+  # レスポンス内容を出力
+  body = "<html><body>\n"
+  body += "#{Time.new}"
+  body += "</body></html>\n"
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
 server.start
