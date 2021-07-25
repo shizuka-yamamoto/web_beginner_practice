a26b3fec4065742a773699f6a29a48096b7eacb2
shizuka <shiiishim920@gmail.com>
2021-07-25 22:59:39 +0900

    Webサーバを立ち上げてWebの仕組みを知る（フォーム編）

--- webrick.rb
+++ webrick.rb
@@ -14,6 +14,23 @@ server.mount_proc("/time") do |req, res|
   res.body = body
 end
 
+server.mount_proc("/form_post") do |req, res|
+  h = req.query
+  puts h
+  body = "<html><body><head><meta charset=\"utf-8\"></head><p>クエリパラメータは#{h}です</p><p>こんにちは#{h["user_name"]}さん。あなたの年齢は#{h["user_age"]}ですね</body></html>" 
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
+
+server.mount_proc("/form_get") do |req, res|
+  h = req.query
+  puts h
+  body = "<html><body><head><meta charset=\"utf-8\"></head><p>クエリパラメータは#{h}です</p><p>こんにちは#{h["user_name"]}さん。あなたの年齢は#{h["user_age"]}ですね</body></html>" 
+  res.status = 200
+  res['Content-Type'] = 'text/html'
+  res.body = body
+end
 
 server.start
 
